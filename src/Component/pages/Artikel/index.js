1a58a659c3ebdce27f18cc0e7493929d641fc217
dimasajiwicaksono <dimasajiwicaksono@gmail.com>
2020-05-06 04:07:52 +0700

    modified

--- src/Component/pages/Artikel/index.js
+++ src/Component/pages/Artikel/index.js
@@ -1,20 +1,62 @@
 import React from 'react'
 import { Typography, Layout, Row, Col, Card, Carousel, Avatar } from 'antd'
 import { settings } from '../../Utils/settings';
-import { data, user } from '../../Data/Data'
 import globalStyles from '../global.module.css'
 import styles from './article.module.css'
-import { RoutePath } from '../../../Route';
 // import ErrorPages from '../Error/ErrorPages'
 
 function Artikel() {
   const { Title } = Typography;
   const { Content } = Layout;
 
-  const handleCard = (event) => {
-    RoutePath.RouteFunc(event)
-  }
 
+const data = [
+  {
+    id: 1,
+    title: 'Pertanian Terkini',
+    desc: 'Habitual action fro daily life',
+    img: 'https://miro.medium.com/max/1200/1*fBrxY5POAa39B7E4fxYrbw.jpeg'
+  },
+  {
+    id: 2,
+    title: 'Data Visualization For Agritech',
+    desc: 'Habitual action fro daily life',
+    img: 'https://miro.medium.com/max/2000/1*INE-ZJA6HUc4JnRblZPzpA.png'
+  },
+  {
+    id: 3,
+    title: 'Sobat Tani',
+    desc: 'Habitual action fro daily life',
+    img: 'https://miro.medium.com/max/1400/1*RldyrFWyMYS5mhvUNmkw7g.jpeg'
+  }
+]
+
+const user = [
+  {
+    id: 1,
+    name: 'Dimas Aji Wicaksono',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    message: 'I love Medium’s new publications OneZero, GEN, Heated. And, I’m especially excited about Tenderly' 
+  },
+  {
+    id: 2,
+    name: 'Fajar Surya Kencana',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    message: 'Dunia tak lagi sama tak selamanya memihak kita, disaat kita mau berusaha di situlah kebahagiaan akan indah pada waktunya.' 
+  },
+  {
+    id: 3,
+    name: 'Jonathan Cristie',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    message: 'Belajarlah rendah hati, rendahkan hatimu serendah-rendahnya hingga tidak ada seorangpun yang bisa merendahkanmu.' 
+  },
+  {
+    id: 4,
+    name: 'Panji Danutirto',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    message: 'Hidup itu sebentar. Kamu harus bisa tersenyum saat merasakan kepedihan atau kita tak akan pernah melanjutkan hidup' 
+  },
+]
   return (
     <div className={globalStyles.layoutBg}>
       {/* <ErrorPages /> */}
@@ -99,7 +141,7 @@ function Artikel() {
                 <Col md={8} xs={24} key={index}>
                   <Card
                     hoverable
-                    onClick={() => handleCard(item.title)}
+                    // onClick={() => handleCard(item.title)}
                     style={{ width: '100%' }}
                     cover={<img alt="example" src={item.img} className={styles.cardImg}/>}
                   >
